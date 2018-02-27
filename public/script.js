3c30f07c97a22e82b795d7cf0c2ed42d9a8bc0d4
snelakanti <vnelakanti@gmail.com>
2018-02-27 15:21:36 -0500

    add data in script.js in the form of array

--- public/script.js
+++ public/script.js
@@ -1,17 +1,2 @@
-// get Generate Button
-var btnGenerate = document.getElementById('generateBtn');
-// get outout div
-var output = document.getElementsByClassName("output")[0];
+console.log("hello");
 
-//add Click Listener
-btnGenerate.addEventListener("click",()=>{
-  var quote = getRandomQuote(quotes.lenght);
-  output.innerHTML = '<p>' + quote.quote + '</p>' + '<p class="w"> - ' + quote.name + '</p>';
-});
-
-//function for Random Quote Generator
-var getRandomQuote = ()=> {
-  var rnumber = Math.floor(Math.random() * quotes.length);
-  console.log(rnumber);
-  return quotes[rnumber];
-}
